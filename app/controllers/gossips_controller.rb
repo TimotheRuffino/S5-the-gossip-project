e34775346d9b101cb714dc041b3566ec94bb76dd
TimotheRuffino <timothe.ruffino@gmail.com>
2020-05-05 23:47:32 +0200

    commit

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,12 +1,25 @@
 class GossipsController < ApplicationController
 
-  def new
-  end
+	def new
+		@gossip = Gossip.new
+	end
 
-  def create
-  end
+	def create
+    @gossip = Gossip.new(user: User.first, title: params[:title], content: params[:content])
+    #title: params[:title], content: params[:content], user: params[:user] # avec xxx qui sont les données obtenues à partir du formulaire
 
-  def index
+	if @gossip.save # essaie de sauvegarder en base @gossip
+    redirect_to gossips_path
+  else
+   render 'new' # sinon, il render la view new (qui est celle sur laquelle on est déjà)
   end
-  
+	end
+
+	def show
+		@gossip = Gossip.find(params[:id])
+	end
+
+	def index
+		@gossip = Gossip.all
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root 'root#index'
   resources :gossips
-  get '/', to: 'static#index'
-  get '/team', to: 'static#team'
-  get '/contact', to: 'static#contact'
-  get '/welcome/:id', to: 'static#welcome'
-  get root 'static#index'
-  get '/show', to: 'static#show'
-  get '/show/:id', to: 'static#show'
+
+  resources :welcome, only: [:index]
+  resources :contact, only: [:index]
+  resources :team, only: [:index]
+  resources :root, only: [:index]
 end
